fcf50f2a5cfc2672e530e6c873228a0b2d9c6a29
mario martinez <amakenadog@gmail.com>
2013-06-26 20:16:50 -0600

    created users in bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,7 +6,10 @@ class BootStrap {
     def init = { servletContext ->
     	switch(GrailsUtil.environment) {
     		case "development":
-
+    			def u1 = User.build(role:"admin")
+    			def u2 = User.build()
+    			def u3 = User.build()
+    			def u4 = User.build()
     		break
     	}
     }
