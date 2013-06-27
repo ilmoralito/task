fa9108d6202412eb4f22fad2e3fe65c81e1978a2
mario martinez <amakenadog@gmail.com>
2013-06-26 21:23:34 -0600

    created application domain class and controller

---
+++ grails-app/controllers/ni/edu/uccleon/ApplicationController.groovy
@@ -0,0 +1,12 @@
+package ni.edu.uccleon
+
+class ApplicationController {
+
+	static defaultAction = ""
+	static allowedMethods = []
+
+    def index() {
+
+    }
+
+}
---
+++ grails-app/domain/ni/edu/uccleon/Application.groovy
@@ -0,0 +1,20 @@
+package ni.edu.uccleon
+
+class Application {
+
+	Date dateCreated
+	Date lastUpdated
+
+    static constraints = {
+
+    }
+
+    static mapping = {
+    	verison false
+    }
+
+    String toString() {
+    	//
+    }
+
+}
