c0e7280dd0cfbd11e4ea1ac7c2df119fb3c19e8e
mario martinez <amakenadog@gmail.com>
2013-06-27 09:47:09 -0600

    added update password feature

--- grails-app/controllers/ni/edu/uccleon/UserController.groovy
+++ grails-app/controllers/ni/edu/uccleon/UserController.groovy
@@ -1,12 +1,15 @@
 package ni.edu.uccleon
 
+import org.springframework.web.context.request.RequestContextHolder as RCH
+
 class UserController {
 
 	static defaultAction = "login"
 	static allowedMethods = [
 		login:["GET", "POST"],
 		logout:"GET",
-		profile:["GET", "POST"]
+		profile:["GET", "POST"],
+		updatePassword:"POST"
 	]
 
     def login(String email, String password) {
@@ -50,5 +53,52 @@ class UserController {
 
     	[user:User.findByEmail(session?.user?.email)]
     }
+
+    def updatePassword(updatePasswordCommand cmd) {
+    	if (!cmd.validate()) {
+    		flash.message = "wrong.data"
+    		chain action:"profile", model:[cmd:cmd]
+    		return
+    	}
+
+    	def user = cmd.updateUserPassword()
+
+    	if (user) {
+    		flash.message = "user.password.updated"
+    	} else {
+    		flash.message = "something.when.wrong"
+    	}
+
+    	redirect action:"profile"
+    }
+
+}
+
+class updatePasswordCommand {
+	String password
+	String nPassword
+	String rPassword
+
+	static constraints = {
+		importFrom User
+		def session = RCH.requestAttributes.session
+
+		password blank:false, validator:{password ->
+			password.encodeAsSHA1() == session?.user?.password
+		}
+		rPassword blank:false, validator:{rPassword, obj ->
+			rPassword == obj.nPassword
+		}
+	}
+
+	User updateUserPassword() {
+		def user = User.findByEmail(session?.user?.email)
+
+		if (user) {
+			user.properties["password"] = nPassword
+		}
+
+		user
+	}
 
 }
