291186550d39291ff1d9030c2c2884d033d8d515
mario martinez <amakenadog@gmail.com>
2013-06-26 21:34:50 -0600

    set default password for two users in bootstrap.groovy file

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -6,8 +6,8 @@ class BootStrap {
     def init = { servletContext ->
     	switch(GrailsUtil.environment) {
     		case "development":
-    			def u1 = User.build(role:"admin")
-    			def u2 = User.build()
+    			def u1 = User.build(role:"admin", password:"123")
+    			def u2 = User.build(password:"123")
     			def u3 = User.build()
     			def u4 = User.build()
     		break
