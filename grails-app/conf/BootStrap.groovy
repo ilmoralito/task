ec373419643b5a2f01e7f277488db96d89390412
mario martinez <amakenadog@gmail.com>
2013-07-01 09:23:02 -0600

    created more dummy users applications

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -9,9 +9,14 @@ class BootStrap {
                 //users
                 def u1 = User.build(password:"123", enabled:true, role:"admin")
                 def u2 = User.build(enabled:true)
+                def u3 = User.build(enabled:true, password:"123", department:"Registro")
+                def u4 = User.build(enabled:true, password:"123", department:"Recursos Humanos")
 
     			def app1 = Application.build(department:"Soporte Tecnico", user:u1)
                 def app2 = Application.build(department:"Administracion", user:u2)
+
+                def app3 = Application.build(department:"Soporte Tecnico", user:u3)
+                def app4 = Application.build(department:"Soporte Tecnico", user:u4)
     		break
     	}
     }
