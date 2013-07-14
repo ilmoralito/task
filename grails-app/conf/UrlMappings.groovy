1afa5ad7ccce3101ed7a01b96aa53bb31e49d8de
mario martinez <amakenadog@gmail.com>
2013-07-14 12:11:02 -0600

    updated urlmapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -3,11 +3,70 @@ class UrlMappings {
 	static mappings = {
 		"/$controller/$action?/$id?"{
 			constraints {
-				// apply constraints here
+				id matches:/\d+/
 			}
 		}
 
-		"/"(view:"/index")
+		//root
+		"/"(controller:"user", action:"login")
+
+		//user
+		"/login" {
+			controller = "user"
+			action = "login"
+		}
+
+		"/logout" {
+			controller = "user"
+			action = "logout"
+		}
+
+		"/profile" {
+			controller = "user"
+			action = "profile"
+		}
+
+		"/users" {
+			controller = "user"
+			action = "list"
+		}
+
+		//application
+		"/myapplications/$state" {
+			controller = "application"
+			action = "list"
+			constraints {
+				state inList:["pending", "attending", "attended", "done"]
+			}
+		}
+
+		"/applications/$state" {
+			controller = "application"
+			action = "pendingApplications"
+			constraints {
+				state inList:["pending", "attending", "attended"]
+			}
+		}
+
+		//task
+		"/deleteTask/$id/$applicationId" {
+			controller = "task"
+			action = "delete"
+			constraints {
+				id matches:/\d+/
+				applicationId matches:/\d+/
+			}
+		}
+
+		"/updateTaskStatus/$id/$applicationId" {
+			controller = "task"
+			action = "updateState"
+			constraints {
+				id matches:/\d+/
+				applicationId matches:/\d+/
+			}
+		}
+
 		"500"(view:'/error')
 	}
 }
